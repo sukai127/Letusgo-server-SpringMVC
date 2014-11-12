4d816449b9dc0ccec91565c27fa9e480768b0c22
SnakerWorker <snakerworker@gmail.com>
2014-11-12 20:34:07 +0800

    add fields

--- src/main/java/com/thougthworks/letusgo/model/Item.java
+++ src/main/java/com/thougthworks/letusgo/model/Item.java
@@ -1,7 +1,70 @@
 package com.thougthworks.letusgo.model;
 
-/**
- * Created by sukai on 11/12/14.
- */
 public class Item {
+    private int id;
+    private String barcode;
+    private String name;
+    private String unit;
+    private double price;
+    private Category category;
+
+    public Item() {
+    }
+
+    public Item(int id, String barcode, String name, String unit, double price, Category category) {
+        this.id = id;
+        this.barcode = barcode;
+        this.name = name;
+        this.unit = unit;
+        this.price = price;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
