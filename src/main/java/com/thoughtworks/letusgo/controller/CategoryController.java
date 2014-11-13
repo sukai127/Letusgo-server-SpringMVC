3aac91e8fef1b8431bac4de45cd63f2164c47356
fgcui <fgcui1204@163.com>
2014-11-13 23:44:57 +0800

    add getCategories method

--- src/main/java/com/thoughtworks/letusgo/controller/CategoryController.java
+++ src/main/java/com/thoughtworks/letusgo/controller/CategoryController.java
@@ -1,7 +1,23 @@
 package com.thoughtworks.letusgo.controller;
 
-/**
- * Created by cuifugang on 14-11-13.
- */
+import com.thoughtworks.letusgo.model.Category;
+import com.thoughtworks.letusgo.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryServiceImpl;
+
+    @RequestMapping(value = "/categories" , method = RequestMethod.GET)
+    public List<Category> getCategories(){
+        return categoryServiceImpl.getCategories();
+    }
 }
