c2d28eaff34456026274607287882e4e85e175e7
SnakerWorker <snakerworker@gmail.com>
2014-11-13 10:34:23 +0800

    add getCategories method

--- src/main/java/com/thoughtworks/letusgo/dao/CategoryDaoImpl.java
+++ src/main/java/com/thoughtworks/letusgo/dao/CategoryDaoImpl.java
@@ -1,7 +1,32 @@
 package com.thoughtworks.letusgo.dao;
 
-/**
- * Created by sukai on 11/13/14.
- */
+import com.thoughtworks.letusgo.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
 public class CategoryDaoImpl {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<Category> getCategories(){
+
+        List<Category> categories;
+        String sql = "select * from category";
+
+        categories = jdbcTemplate.query(sql,new RowMapper<Category>() {
+            @Override
+            public Category mapRow(ResultSet resultSet, int i) throws SQLException {
+                return new Category(resultSet.getInt("id"),resultSet.getString("name"));
+            }
+        });
+        return categories;
+    }
 }
