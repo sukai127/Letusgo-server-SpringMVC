be5a1aea8998895a46a23f925f8529e55743082c
SnakerWorker <snakerworker@gmail.com>
2014-11-13 22:49:30 +0800

    add test should_insert_item

--- src/test/java/com/thoughtworks/letusgo/controller/ItemControllerTest.java
+++ src/test/java/com/thoughtworks/letusgo/controller/ItemControllerTest.java
@@ -4,33 +4,28 @@ import com.google.common.collect.ImmutableList;
 import com.thoughtworks.letusgo.model.Category;
 import com.thoughtworks.letusgo.model.Item;
 import com.thoughtworks.letusgo.service.ItemService;
-import com.thoughtworks.letusgo.service.ItemServiceImpl;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml"})
@@ -79,5 +74,22 @@ public class ItemControllerTest {
         mockMvc.perform(delete("/api/items/1"))
                 .andExpect(status().is(204));
         verify(itemService, times(1)).deleteItem(1);
+    }
+
+    @Test
+    public void should_insert_item() throws Exception {
+        String requestBody = "{\"id\":1, \"barcode\":\"ITEM000001\", " +
+                "\"name\":\"苹果\", " +
+                "\"price\":3.5, " +
+                "\"unit\":\"斤\", " +
+                "\"category\":{\"id\":1,\"name\":\"水果\"}}";
+        mockMvc.perform(post("/api/items")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isCreated());
+
+        ArgumentCaptor<Item> item = ArgumentCaptor.forClass(Item.class);
+        verify(itemService, times(1)).insertItem(item.capture());
     }
 }
