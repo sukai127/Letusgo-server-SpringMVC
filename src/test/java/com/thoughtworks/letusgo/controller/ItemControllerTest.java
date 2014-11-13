79c42bc9ab473e16aaab0ffe0ac7656f5447a42d
SnakerWorker <snakerworker@gmail.com>
2014-11-13 20:43:59 +0800

    add test should_return_item_when_input_id

--- src/test/java/com/thoughtworks/letusgo/controller/ItemControllerTest.java
+++ src/test/java/com/thoughtworks/letusgo/controller/ItemControllerTest.java
@@ -6,17 +6,27 @@ import com.thoughtworks.letusgo.model.Item;
 import com.thoughtworks.letusgo.service.ItemService;
 import com.thoughtworks.letusgo.service.ItemServiceImpl;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -31,26 +41,38 @@ public class ItemControllerTest {
     private MockMvc mockMvc;
     private ImmutableList<Item> itemList;
 
+    @Mock
     private ItemService itemService;
 
-    @SuppressWarnings("SpringJavaAutowiringInspection")
-    @Autowired
-    protected WebApplicationContext wac;
+    @InjectMocks
+    private ItemController itemController;
 
     @Before
     public void init(){
-        itemService = mock(ItemServiceImpl.class);
-        mockMvc = webAppContextSetup(this.wac).build();
+
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(itemController).build();
+
         itemList = ImmutableList.of(new Item(1, "ITEM000001", "苹果", "斤", 3.5, new Category(1, "水果")),
                 new Item(2, "ITEM000002", "可乐", "瓶", 3.5, new Category(2, "饮料")),
                 new Item(3, "ITEM000003", "鞋", "双", 95, new Category(3, "服装")));
         when(itemService.getItems()).thenReturn(itemList);
+        when(itemService.getItem(1)).thenReturn(itemList.get(0));
     }
 
     @Test
     public void should_return_itemList() throws Exception {
         mockMvc.perform(get("/api/items"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].name",is("苹果")));
+                .andExpect(jsonPath("$",hasSize(3)))
+                .andExpect(jsonPath("$[0].name", is("苹果")));
+    }
+
+    @Test
+    public void should_return_item_when_input_id() throws Exception {
+        mockMvc.perform(get("/api/items/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(1)))
+                .andExpect(jsonPath("$.name", is("苹果")));
     }
 }
