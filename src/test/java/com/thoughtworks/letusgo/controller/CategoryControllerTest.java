e27b3084f4cae0691930406e69605a6e2699a154
fgcui <fgcui1204@163.com>
2014-11-13 23:40:22 +0800

    add test should_return_categoryList method

--- src/test/java/com/thoughtworks/letusgo/controller/CategoryControllerTest.java
+++ src/test/java/com/thoughtworks/letusgo/controller/CategoryControllerTest.java
@@ -2,9 +2,9 @@ package com.thoughtworks.letusgo.controller;
 
 import com.google.common.collect.ImmutableList;
 import com.thoughtworks.letusgo.model.Category;
-import com.thoughtworks.letusgo.model.Item;
 import com.thoughtworks.letusgo.service.CategoryService;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -15,7 +15,12 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml"})
@@ -41,8 +46,16 @@ public class CategoryControllerTest {
                 new Category(2,"饮料"),
                 new Category(3,"服装")
         );
-        
+
         when(categoryService.getCategories()).thenReturn(categoryList);
         when(categoryService.getCategory(1)).thenReturn(categoryList.get(0));
+    }
+
+    @Test
+    public void should_return_categoryList() throws Exception {
+        mockMvc.perform(get("/api/categories"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(3)))
+                .andExpect(jsonPath("$[0].name", is("水果")));
     }
 }
