fc55235ef36d7d252985a44d46b6279d7ae6c1d0
SnakerWorker <snakerworker@gmail.com>
2014-11-13 11:26:42 +0800

    add getCartItems method

--- src/main/java/com/thoughtworks/letusgo/dao/CartItemDaoImpl.java
+++ src/main/java/com/thoughtworks/letusgo/dao/CartItemDaoImpl.java
@@ -1,7 +1,30 @@
 package com.thoughtworks.letusgo.dao;
 
-/**
- * Created by sukai on 11/13/14.
- */
+import com.thoughtworks.letusgo.model.CartItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
 public class CartItemDaoImpl {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<CartItem> getCartItems(){
+
+        List<CartItem> cartItems = null;
+        String sql = "select * from cartitem";
+        cartItems = jdbcTemplate.query(sql,new RowMapper<CartItem>() {
+            @Override
+            public CartItem mapRow(ResultSet resultSet, int i) throws SQLException {
+                return new CartItem(resultSet.getInt("id"),null,resultSet.getInt("count"));
+            }
+        });
+        return cartItems;
+    }
 }
