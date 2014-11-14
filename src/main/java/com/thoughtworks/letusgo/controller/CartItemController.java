b3a73d230a25b81ee92e3f9d9cbb68b52a0bac6d
fgcui <fgcui1204@163.com>
2014-11-14 09:35:38 +0800

    add getCartItems method

--- src/main/java/com/thoughtworks/letusgo/controller/CartItemController.java
+++ src/main/java/com/thoughtworks/letusgo/controller/CartItemController.java
@@ -1,7 +1,22 @@
 package com.thoughtworks.letusgo.controller;
 
-/**
- * Created by cuifugang on 14-11-14.
- */
+import com.thoughtworks.letusgo.model.CartItem;
+import com.thoughtworks.letusgo.service.CartItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class CartItemController {
+    @Autowired
+    private CartItemService cartItemService;
+
+    @RequestMapping(value = "/cartItems" ,method = RequestMethod.GET)
+    public List<CartItem> getCartItems(){
+        return cartItemService.getCartItems();
+    }
 }
